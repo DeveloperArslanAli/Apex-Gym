63ef733461bd8e91808a00d4741996c2e4542e66
DeveloperArslanAli <arslanali543.am@gmail.com>
2026-07-27 01:14:13 +0500

    Update download script and pre-compiled APK binary with LAN IP

--- mobile/build_suite/download-eas-apk.js
+++ mobile/build_suite/download-eas-apk.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-const url = "https://expo.dev/artifacts/eas/K6Hg_nvLjC6w9dSvqeq1K4f9qDoUaNwEh-VUSW6Jzis.apk";
+const url = "https://expo.dev/artifacts/eas/H_9ZWiu6l7hHHUiEFxu8F7KPj5_XtWIlOdC8sZ-Ztn8.apk";
 const dest = path.join(__dirname, 'APEX-Gym-Release.apk');
 
 console.log('Downloading APEX-Gym standalone APK from Expo dev servers...');
